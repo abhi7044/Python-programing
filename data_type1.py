961552b0557690b660bdc7da242db51a92e9d0e8
Abhijeet Mishra <teslanikola664@gmail.com>
2025-12-27 20:34:05 +0530

    Add files via upload

---
+++ data_type1.py
@@ -0,0 +1,15 @@
+#our first program 
+age = 35
+PI = 3.14
+
+num = 2
+isPrime =True
+
+print(type(age))
+print(type(PI))
+print(type(isPrime))  #coments 
+
+'''
+this is a 
+multiline comments
+'''
---
+++ operator1.4.py
@@ -0,0 +1,31 @@
+# Arithmetic Operator
+a = 10 
+b = 5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+
+# Relational Operator
+print(a<b)
+
+# Assignment Operator
+a += 5
+print(a)
+
+# Logical Operator
+var = False
+
+print(not var)
+print((5>3) and (3>2))
+print((5>3) and (3>8))
+print((5>3) or (3>8))
+
+
+# question 
+x = 3
+x += 5
+print(x)
