bca21e609e80cf4853604b7c39c1ab3496ce45a0
Abhijeet Mishra <teslanikola664@gmail.com>
2026-01-22 01:31:31 +0530

    Add files via upload

---
+++ Indexing.py
@@ -0,0 +1,8 @@
+word = "Python"
+
+# word[2] = 'h'    This line give error because in python the strings are immutable ===> immutable means we can't change randomle any word
+
+print(word[2])
+
+for ch in word:
+    print(ch)
