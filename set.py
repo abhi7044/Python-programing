95db941c967c5145f01397f007c9bdd60adb7a17
Abhijeet Mishra <teslanikola664@gmail.com>
2026-01-26 20:04:53 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,20 @@
+s = {1, 2, 2, 2, 3}
+
+# empty_set = set()
+
+# s.add(5)
+# s.remove(1)
+# print(type(empty_set))
+
+# s.clear()
+# print(s)
+
+print(s)
+s.pop()
+print(s)
+
+s1 = {1, 2, 3, 4, 5}
+s2 = {4, 5, 8, 9, 10}
+
+print(s1.union(s2))
+print(s1.intersection(s2))
